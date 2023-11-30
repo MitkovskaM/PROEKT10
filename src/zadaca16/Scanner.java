d17943ef0efefd593c4a0d149a682da429eab10e
MitkovskaM <mihaela.mitkovska@gmail.com>
2023-11-30 10:38:17 +0100

    Add first java proekt

---
+++ src/zadaca16/Scanner.java
@@ -0,0 +1,10 @@
+package zadaca16;
+
+public class Scanner {
+
+	public int nextInt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/zadaca16/deliviso4i7.java
@@ -0,0 +1,18 @@
+package zadaca16;
+
+public class deliviso4i7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner tastatura = new Scanner();
+
+	        System.out.print("Внесете големина на низата (n): ");
+	        int n = tastatura.nextInt();
+	        System.out.println("clenovi vo nizata koi se delivi so 7 i 4:");
+	        for(int[] i=1, 1<=n, i++);
+	        		if(i%4==0&&i%7==0) {
+	        			
+	        		}
+	}
+
+}
